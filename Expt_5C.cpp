54b1ecc2a3ba69dfba52013fbea49d94d25551f3
Abhisar Behera <behera.abhisar@gmail.com>
2025-07-24 00:13:57 +0530

    Update Expt_5C.cpp

--- Expt_5C.cpp
+++ Expt_5C.cpp
@@ -3,41 +3,31 @@
 using namespace std;
 
 int main() {
-    int num_1, num_2, num_3;
+    char character;
 
-    cout << "Enter first number: ";
-    cin >> num_1;
-    cout << "Enter second number: ";
-    cin >> num_2;
-    cout << "Enter third number: ";
-    cin >> num_3;
+    cout << "Enter a character: ";
+    cin >> character;
 
-    if(num_1 > num_2) {
-        if(num_1 > num_3) {
-            cout << num_1 << " is the largest number" << endl;
-        } else {
-            cout << num_3 << " is the largest number" << endl;
+    char char_array[10] = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+
+    for(int i = 0; i < sizeof(char_array)/sizeof(char); i++) {
+        if (character == char_array[i]) {
+            cout << "The given character " << character << " is a vowel" << endl;
+            return 0;
         }
-    } else if (num_2 > num_3)
-    {
-        cout << num_2 << " is the largest number" << endl;
-    } else {
-        cout << num_3 << " is the largest number" << endl;
     }
+
+    cout << "The given character " << character << " is a consonant" << endl;
 
     return 0;
 }
 
 /* Output
 
-Enter first number: 2
-Enter second number: 5
-Enter third number: 4
-5 is the largest number
+Enter a character: a
+The given character a is a vowel
 
-Enter first number: 6
-Enter second number: 3
-Enter third number: 1
-6 is the largest number
+Enter a character: r
+The given character r is a consonant
 
 */
